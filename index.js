a2bca31bad22b7a9c24b161d3fcdf781052fb2df
Sebastian Hönel <development@hoenel.net>
2018-08-07 19:53:37 +0200

    Import/export AngularSimpleWebManager in index.js and add sub-package initialization to package.json

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { SevenZipTaskResolvedConfig, SevenZipTask, SevenZipTaskConfigSchema } = require('./lib/tasks/7ZipTask')
+, { AngularSimpleWebManager, AngularSimpleWebManagerSchema } = require('./lib/managers/angular-simple-web')
 , { macRegex, wakeAsync} = require('./lib/tools/WakeOnLan');
 
 
 module.exports = Object.freeze({
   SevenZipTaskResolvedConfig, SevenZipTask, SevenZipTaskConfigSchema,
+  AngularSimpleWebManager, AngularSimpleWebManagerSchema,
 
   macRegex, wakeAsync
 });
