f99ac3e295c97e86abf07f2e5f21f1a52d78e501
Sebastian Hönel <development@hoenel.net>
2018-08-17 00:54:38 +0200

    Update typedefs to make explicit that the ResolvedResolveObject and the task is passed to every property

--- meta/typedefs.js
+++ meta/typedefs.js
@@ -13,15 +13,15 @@
 /**
  * @typedef SevenZipTaskConfigBase
  * @type {Object}
- * @property {SevenZipTaskBackupMode|(() => SevenZipTaskBackupMode|Promise.<SevenZipTaskBackupMode>)} [backupMode] Optional. Defaults to 'zip'.
- * @property {string|(() => string|Promise.<string>)} sevenZip The absolute path to the 7z executable.
- * @property {string|(() => string|Promise.<string>)} src
- * @property {string|(() => string|Promise.<string>)} dstFolder Also supports substitutions.
- * @property {string|(() => string|Promise.<string>)} [dstFile] Optional. Defaults to null. May only be null if backup-mode is 'copy'. Supports substitutions.
- * @property {boolean|(() => boolean|Promise.<boolean>)} [mkdirpDstFolder] Optional. Defaults to true. Whether or not to try to recursively create the destination directory before running the backup.
- * @property {boolean|(() => boolean|Promise.<boolean>)} [emptyDstBefore] Optional. Defaults to false. Whether or not to empty the destination folder before running the backup.
- * @property {string|(() => string|Promise.<string>)} [password] Optional. Defaults to null.
- * @property {Array.<string>|(() => Array.<string>|Promise.<Array.<string>>)} [sevenZipArgs] Optional. Defaults to [].
+ * @property {SevenZipTaskBackupMode|((rro: ResolvedResolveObject, task: Task) => SevenZipTaskBackupMode|Promise.<SevenZipTaskBackupMode>)} [backupMode] Optional. Defaults to 'zip'.
+ * @property {string|((rro: ResolvedResolveObject, task: Task) => string|Promise.<string>)} sevenZip The absolute path to the 7z executable.
+ * @property {string|((rro: ResolvedResolveObject, task: Task) => string|Promise.<string>)} src
+ * @property {string|((rro: ResolvedResolveObject, task: Task) => string|Promise.<string>)} dstFolder Also supports substitutions.
+ * @property {string|((rro: ResolvedResolveObject, task: Task) => string|Promise.<string>)} [dstFile] Optional. Defaults to null. May only be null if backup-mode is 'copy'. Supports substitutions.
+ * @property {boolean|((rro: ResolvedResolveObject, task: Task) => boolean|Promise.<boolean>)} [mkdirpDstFolder] Optional. Defaults to true. Whether or not to try to recursively create the destination directory before running the backup.
+ * @property {boolean|((rro: ResolvedResolveObject, task: Task) => boolean|Promise.<boolean>)} [emptyDstBefore] Optional. Defaults to false. Whether or not to empty the destination folder before running the backup.
+ * @property {string|((rro: ResolvedResolveObject, task: Task) => string|Promise.<string>)} [password] Optional. Defaults to null.
+ * @property {Array.<string>|((rro: ResolvedResolveObject, task: Task) => Array.<string>|Promise.<Array.<string>>)} [sevenZipArgs] Optional. Defaults to [].
  * @property {SimpleTaskConfig|((rro: ResolvedResolveObject) => (SimpleTaskConfig|Promise.<SimpleTaskConfig>))} [tasksAfter] Optional. Defaults to an empty Array. The SevenZipTask creates all of its tasks automatically and prepends them to may existing tasks in the tasks-array. However, some tasks you may want to run afterwards. Those should be put in this array.
  */
 
